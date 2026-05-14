cd4ac247164f099c579417b000b8ed2c7cffd23b
Gabrielle Wang <gaw004@ucsd.edu>
2026-05-13 21:54:37 -0700

    testing branch: should be wrong

--- code-to-unit-test/unit-test-me.js
+++ code-to-unit-test/unit-test-me.js
@@ -1,6 +1,6 @@
 // matches valid phone numbers
 export function isPhoneNumber(phoneNumber) {
-  const phoneRegex = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/;
+  const phoneRegex = /^((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}$/;
   return phoneRegex.test(phoneNumber);
 }
 
